a7773732f96c5e039bf476c1dd65695123325ab8
jamonzondc <54319358+jamonzondc@users.noreply.github.com>
2020-01-08 12:14:09 -0500

    Comente la ruta en app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 //Como los states
-const appRoutes: Routes = [
+/*const appRoutes: Routes = [
   { path: '', redirectTo: 'sigin', pathMatch: 'full' },
   { path: 'dashboard', loadChildren:()=> import('./dashboard/dashboard.module').then(mod=> mod.DashboardModule)},
   { path: '**', redirectTo: 'sigin', pathMatch: 'full' }
-];
+];*/
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes,  { enableTracing: true })],
